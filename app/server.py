8f6a4f86e041ac47442ed6b0eb49de52f1f04276
chetan-patil-altizon <chetan_patil@altizon.com>
2024-08-02 14:48:27 +0530

    created an api to analyze images

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://doc-04-28-docs.googleusercontent.com/docs/securesc/6fe50e0sgnrsjfkk85d2118mdreel1k2/vgeo4mb5ipmse81inja04fspurb583mc/1562565600000/08878578932533926478/08878578932533926478/1kJxTwbi29RgBCsVLZpEF3BB-mol5tDiK?e=download&nonce=tdgnhvptg69jq&user=08878578932533926478&hash=5709gfm4leoltm2egvchr1e2nnmgmpj2'
-model_file_name = 'model'
+model_file_url = 'https://therma.blob.core.windows.net/therma/model.pkl'
+model_file_name = 'model.pkl'
 classes = ['dog', 'fox']
 path = Path(__file__).parent
 
@@ -25,12 +25,17 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        tfms=get_transforms(), size=224).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet34, pretrained=False)
-    learn.load(model_file_name)
-    return learn
+    try:
+        learn = load_learner(f'{path}/models/', model_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
@@ -47,7 +52,9 @@ async def analyze(request):
     data = await request.form()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    return JSONResponse({'result': learn.predict(img)[0]})
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+    
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
